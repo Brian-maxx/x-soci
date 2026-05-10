5d0f23ebb6a0bedd06cadbd9584d2552a3c16f93
brian-maxx <buitantrung01@gmail.com>
2026-05-10 10:02:07 +0700

    init java

--- backend/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.http import JsonResponse
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('products.urls')),
-    path('api/', include('users.urls')),
-]
