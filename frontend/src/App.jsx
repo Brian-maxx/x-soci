c48b510f8fcdb784b819f9974e7795254e086b15
trungbt <trungbt@vn.ids.jp>
2026-05-23 12:58:16 +0700

    Revert "Merge pull request #5 from Brian-maxx/feature/fe-admin"
    This reverts commit c435aeb38c648c44e8271f81559235bb63b5e356, reversing
    changes made to 478f1d0f0fb54f18b3ccec3d807940708569074e.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,48 @@
 import { Routes, Route, Navigate, useLocation  } from "react-router-dom";
 import './App.css';
-import UserLayout from "./layout/UserLayout";
+import Login from "./pages/Auth";
+import Header from './components/Header';
+import Social from './pages/Social';
+import Explore from './pages/Explore';
+import AiChat from './pages/AiChat';
+import Practice from './pages/Practice';
+import PageWrapper from "./components/PageWrapper";
 
 function App() {
   const location = useLocation();
   return (
-      <UserLayout />
+      <div className="app-container">
+        <Routes location={location} key={location.pathname}> 
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/" element={<Header />}>
+            <Route path="social" element={
+              <PageWrapper>
+                <Social />
+              </PageWrapper>
+            } 
+            />
+            <Route path="explore" element={
+              <PageWrapper>
+                <Explore />
+              </PageWrapper>
+            } />
+            <Route path="ai-chat" element={
+              <PageWrapper>
+                <AiChat />
+              </PageWrapper>
+            } />
+            <Route path="practice" element={
+              <PageWrapper>
+                <Practice />
+              </PageWrapper>
+            } />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/social" replace />} />
+        </Routes>
+      </div>
   );
 }
 
---
+++ frontend/src/components/ForgotPassword.jsx
@@ -0,0 +1,29 @@
+import '../styles/Auth.css';
+
+function ForgotPassword({ onSwitch }) {
+    return (
+        <div className='forgot-password-container'>
+            <h1 className='forgot-password-title'>Quên mật khẩu</h1>
+            <form className='forgot-password-form'>
+                <div className='forgot-password-form-container'>
+                    <div className='forgot-password-input-container'>
+                        <label className='forgot-password-label'>Email</label>
+                        <input type='email' placeholder='Email' className='forgot-password-input' />
+                    </div>
+                </div>
+
+                <div className='forgot-password-btn-container'>
+                    <button type='submit' className='forgot-password-button'>Gửi</button>
+                    <div className='forgot-password-login-container'>
+                        <p>Đã có tài khoản?</p>
+                        <button type='button' onClick={onSwitch} className='login-link'>
+                            Đăng nhập
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default ForgotPassword;
